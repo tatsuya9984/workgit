ebb84922e0314dac44991ba0ae94c9b539729a53
tatsuya9984 <tatsuya9984.yahoo.co.jp>
2020-01-13 00:07:48 +0900

    基本形のWEBアプリ完成

--- src/main/java/com/websystem/controller/RootController.java
+++ src/main/java/com/websystem/controller/RootController.java
@@ -1,14 +1,24 @@
 package com.websystem.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.websystem.db.noteRepository;
+import com.websystem.entity.db.NoteEntity;
+
 @Controller
 public class RootController {
+  @Autowired
+  private noteRepository noteRepo;
+
 	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String rootController(Model model, HttpServletRequest req) {
 		HttpSession session = req.getSession();
@@ -16,7 +26,9 @@ public class RootController {
 		if (id == null) {
 			return "redirect:login";
 		}
+		List<NoteEntity> items = noteRepo.findAll();
 		model.addAttribute("name", id);
+		model.addAttribute("items", items);
 		return "top";
 	}
 }
---
+++ src/main/java/com/websystem/entity/db/FavoriteEntity.java
@@ -0,0 +1,47 @@
+package com.websystem.entity.db;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "favorite")
+public class FavoriteEntity {
+
+  @Id
+  @Column(name = "note_id")
+  private String noteId;
+
+  @Column(name = "user_id")
+  private String userId;
+
+  @Column(name = "favorite_day")
+  private Date favoriteDay;
+
+  public String getNoteId() {
+    return noteId;
+  }
+
+  public void setNoteId(String noteId) {
+    this.noteId = noteId;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public Date getFavoriteDay() {
+    return favoriteDay;
+  }
+
+  public void setFavoriteDay(Date favoriteDay) {
+    this.favoriteDay = favoriteDay;
+  }
+}

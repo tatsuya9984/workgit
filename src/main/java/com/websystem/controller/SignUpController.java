06e02c94d50ba54f743f5ed84477e3c7feb04f5a
tatsuya9984 <tatsuya9984.yahoo.co.jp>
2020-01-13 12:20:40 +0900

    UI調整とSignUp追加

--- src/main/java/com/websystem/controller/SignUpController.java
+++ src/main/java/com/websystem/controller/SignUpController.java
@@ -1,36 +1,77 @@
 package com.websystem.controller;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.websystem.db.AuthRepository;
+import com.websystem.db.UserRepository;
+import com.websystem.entity.db.AuthEntity;
+import com.websystem.entity.db.UserEntity;
 
-
+@Controller
 public class SignUpController {
-	private Map<String,String> radioSex;
-	 
-	private Map<String,String> initRadioSex(){
-		Map<String,String> radio = new LinkedHashMap<>();
-		radio.put("女性", "true");
-		radio.put("男性", "false");
+  @Autowired
+  private AuthRepository authRepo;
+  @Autowired
+  private UserRepository userRepo;
  
-		return radio;
-	}
+  @GetMapping("/signup")
+  public String getSignUp(Model model, HttpServletRequest req) {
+    return "signup";
+  }
  
-	@GetMapping("/signup")
-	public String getSignUp(Model model) {
-		radioSex = initRadioSex();
-		model.addAttribute("radioSex",radioSex);
- 
-		return "login/signup";
-	}
- 
-	@PostMapping("/signup")
-	public String postSignUp(Model model) {
-		return "redirect:/login";
-	}
+  @PostMapping("/signup")
+  public String postSignUp(Model model, HttpServletRequest req,
+      @RequestParam(name = "user_id", required = true) String userId,
+      @RequestParam(name = "password", required = true) String password,
+      @RequestParam(name = "re_password", required = true) String rePassword,
+      @RequestParam(name = "family_name", required = true) String familyName,
+      @RequestParam(name = "given_name", required = true) String givenName,
+      @RequestParam(name = "mail_address", required = true) String mailAddress,
+      @RequestParam(name = "sendable_flag", required = false) boolean sendableFlag) {
+    if (sendableFlag != true) {
+      sendableFlag = false;
+    }
+    
+    try {
+      if (!password.equals(rePassword)) {
+        throw new Exception("パスワードが一致しませんでした");
+      }
+
+      if (authRepo.findByUserIdIs(userId) != null) {
+        throw new Exception("このユーザーIDはすでに利用されています");
+      }
+      AuthEntity auth = new AuthEntity();
+      auth.setUserId(userId);
+      auth.setPassword(password);
+      authRepo.saveAndFlush(auth);
+
+      UserEntity user = new UserEntity();
+      user.setUserId(userId);
+      user.setFamilyName(familyName);
+      user.setGivenName(givenName);
+      user.setMailAddress(mailAddress);
+      user.setSendableFlag(sendableFlag);
+      userRepo.saveAndFlush(user);
+    } catch (Exception e) {
+      model.addAttribute("errorMsg", e.getMessage());
+      model.addAttribute("user_id", userId);
+      model.addAttribute("fimily_name", familyName);
+      model.addAttribute("given_name", givenName);
+      model.addAttribute("mail_address", mailAddress);
+      model.addAttribute("sendable_flag", sendableFlag);
+      return "signup";
+    }
+    HttpSession session = req.getSession();
+    session.setAttribute("id", userId);
+    return "redirect:";
+  }
 }
 

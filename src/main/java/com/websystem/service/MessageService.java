f460a149a4916d2672853bd3831ddbf4e93b9112
tatsuya9984 <tatsuya9984.yahoo.co.jp>
2020-01-26 16:56:47 +0900

    no message

--- src/main/java/com/websystem/service/MessageService.java
+++ src/main/java/com/websystem/service/MessageService.java
@@ -1,11 +1,14 @@
 package com.websystem.service;
 
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
 
 /**
  * ラインメッセージのハンドルサービス
@@ -21,7 +24,12 @@ public class MessageService {
    * @param lineId メッセージ送信先LINE_ID
    */
   public void pushHelloMessage(String lineId) {
-    lineMessagingClient
-      .pushMessage(new PushMessage(lineId, new TextMessage("ライン連携ありがとう！")));
+    try {
+      BotApiResponse res = lineMessagingClient
+        .pushMessage(new PushMessage(lineId, new TextMessage("ライン連携ありがとう！"))).get();
+      System.out.println(res.getMessage());
+    } catch (InterruptedException | ExecutionException e) {
+      throw new RuntimeException();
+    }
   }
 }
